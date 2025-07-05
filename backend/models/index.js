dc38ca9cfb01e64c018ed7ff89e40e0c187f313b
ORMatheus <matheus.rdeoliveira77@gmail.com>
2025-07-05 18:16:14 -0300

    Passar por cada model e se ele tiver um .associate ele executa

--- backend/models/index.js
+++ backend/models/index.js
@@ -12,13 +12,21 @@ let sequelize;
 sequelize = new sequelize(config.database,config.username,config.password,config);
 
 fs
-    .readdirSync(__dirname);
-    .filter(file =>{
-        return(
-            file.indexOf('.') !==0 && file !==a basename && file.slice(-3);
-        );
-    })
-    .forcEach(file=>{
-        const model = require(path.join(__dirname,file) ) (sequelize,Sequelize.DataTypes)
-    })
+  .readdirSync(__dirname)
+  .filter(file => { 
+    return (
+      file.indexOf('.') !== 0 &&  
+      file !== basename &&         
+      file.slice(-3) === '.js'      
+    );
+  })
+  .forEach(file => { 
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
+Object.keys(db).forEach(modelName => {
+    if(db[modelName].associate){
+        
+    }
+})
