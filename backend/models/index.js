c755b143e3e964ab44a94b607d890e865cd60022
ORMatheus <matheus.rdeoliveira77@gmail.com>
2025-07-05 18:11:58 -0300

    fazer com que cada arquivo na minha pasta models seja acessado pelo meu sequelize

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,7 @@
 //importações 
 const fs= require('fs') ;//modulo (File system);
 const path = require('path'); //modulo path para trabalhar com caminhos de arquivos
-const Sequelize=require('sequelize'); // modulo para trabalhar com o ORM
+const Sequelize = require('sequelize');  // modulo para trabalhar com o ORM
 const basename=path.basename(__filename); // Pega o nome deste arquivo ('index.js').
 
 const config= require(__dirname +  '/../config/database.js');
@@ -12,5 +12,13 @@ let sequelize;
 sequelize = new sequelize(config.database,config.username,config.password,config);
 
 fs
-    .readdirSync(__dirname)
+    .readdirSync(__dirname);
+    .filter(file =>{
+        return(
+            file.indexOf('.') !==0 && file !==a basename && file.slice(-3);
+        );
+    })
+    .forcEach(file=>{
+        const model = require(path.join(__dirname,file) ) (sequelize,Sequelize.DataTypes)
+    })
 
