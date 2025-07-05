aa48b05b31805238ca06b603a8bc61d876050585
ORMatheus <matheus.rdeoliveira77@gmail.com>
2025-07-05 17:58:17 -0300

    importações que serão base para ler os arquivos a comunicação deles com o ORM

---
+++ backend/models/index.js
@@ -0,0 +1,8 @@
+
+//importações 
+const fs= require('fs') ;//modulo (File system);
+const path = require('path'); //modulo path para trabalhar com caminhos de arquivos
+const Sequelize=require('sequelize'); // modulo para trabalhar com o ORM
+const basename=path.basename(__filename); // Pega o nome deste arquivo ('index.js').
+
+
