5f20ada718b79c268db1a186e569e032041ca8ce
ORMatheus <matheus.rdeoliveira77@gmail.com>
2025-07-08 13:13:10 -0300

    os parametros a ser quardados pelo req.body

--- backend/controllers/admController.js
+++ backend/controllers/admController.js
@@ -2,5 +2,7 @@ const db= require('../models');
 const bcrypt = require('bcryptjs');
 
 exports.createAdm= async(req, res) => {
-    
+    try{
+        const {Nome_admin, Email_Admin, Hash_senha_Admin} = req.body
+    }
 }
