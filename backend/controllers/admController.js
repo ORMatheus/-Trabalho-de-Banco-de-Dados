25e9994d65416b60f07a36d823ac63495a5017c7
ORMatheus <matheus.rdeoliveira77@gmail.com>
2025-07-08 13:15:36 -0300

    Senha de erro e cripografia da senha assim como a classe de criação do Adm

--- backend/controllers/admController.js
+++ backend/controllers/admController.js
@@ -3,6 +3,17 @@ const bcrypt = require('bcryptjs');
 
 exports.createAdm= async(req, res) => {
     try{
-        const {Nome_admin, Email_Admin, Hash_senha_Admin} = req.body
+        const {Nome_admin, Email_Admin, Hash_senha_Admin} = req.body;
+
+        if(!Nome_admin || !Email_Admin || !Hash_senha_Admin){
+            return res.status(400).json({ message: 'Nome, Email e Senha são obrigatórios.' });
+        }
+        const senhaCriptografada = await bcrypt.hash(Hash_Senha_Admin, 10);
+
+        const novoAdm = await db.adm.create({
+            Nome_Admin,
+            Email_Admin,
+            Hash_Senha_Admin: senhaCriptografada,
+        });
     }
 }
